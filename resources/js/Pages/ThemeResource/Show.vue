76526992529f3856b3596f7a8b1b58695afb153b
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2024-03-03 00:27:29 +0000

    translate theme

--- resources/js/Pages/ThemeResource/Show.vue
+++ resources/js/Pages/ThemeResource/Show.vue
@@ -1,6 +1,6 @@
 <template>
     <b-col class="mt-5" cols="12">
-        <h1 class="mb-4">{{ theme.name }}</h1>
+        <h1 class="mb-4">{{ $t('label.' + theme.name.replaceAll(' ', '_')) }}</h1>
         <resource-masonry-with-data-fetching :resources="pagination.data"
                                              :url="route('theme-resources.show', theme.id)"></resource-masonry-with-data-fetching>
     </b-col>
